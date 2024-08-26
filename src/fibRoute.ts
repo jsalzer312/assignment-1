8f4e45a2ffd6b3917f49c5917d46fcfddec935d3
jsalzer312 <salzer.james@yahoo.com>
2024-08-26 11:43:10 -0400

    Update fibRoute.ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -2,10 +2,18 @@
 
 import fibonacci from "./fib";
 
+<<<<<<< Updated upstream
 export default (req, res) => {
   const { num } = req.params;
 
   const fibN = fibonacci(parseInt(num));
+=======
+export default (req,res) => {
+  const num = req.params.num;
+
+  const fibN = fibonacci(num);
+
+>>>>>>> Stashed changes
   let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
